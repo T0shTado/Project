5970203bbb8d020eaf9d81aef5e297da2f4592a3
ToshTado <Marcopafjs@gmail.com>
2022-09-06 13:20:37 -0300

    Project created

---
+++ backend/src/test/java/com/tosh/projeto/salesmetas/SalesmetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tosh.projeto.salesmetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesmetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
