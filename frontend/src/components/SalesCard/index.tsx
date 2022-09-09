37b75011c1e248c308e2f7bed8e2e182e048b8b1
ToshTado <Marcopafjs@gmail.com>
2022-09-09 15:12:46 -0300

    date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,11 +20,17 @@ function SalesCard() {
 
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+        console.log(dmin);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 Setsales(response.data.content);
             });
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-container">
